a8a14d8f24643ca89ba77cec8c667d55215e275c
heindrickdumdum0217 <heindrickd17@gmail.com>
2024-03-26 14:20:01 -0400

    chore: Explicitly set default auto field

--- rest_framework_tracking/apps.py
+++ rest_framework_tracking/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class RestFrameworkTrackingConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
+
     name = "rest_framework_tracking"
     verbose_name = "REST Framework Tracking"
