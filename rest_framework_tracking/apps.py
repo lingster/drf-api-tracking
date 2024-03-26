01210e5e04489f3c12dd525227250204f9a67288
heindrickdumdum0217 <heindrickd17@gmail.com>
2024-03-26 14:08:30 -0400

    feat: Set primary key to BigAutoField

--- rest_framework_tracking/apps.py
+++ rest_framework_tracking/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class RestFrameworkTrackingConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "rest_framework_tracking"
     verbose_name = "REST Framework Tracking"
